c938558ee81ea27ca8b3881409924704eb3bdfbc
Ahmed Ali <aali.97@yahoo.com>
2023-03-23 15:23:24 +0500

    Initial

---
+++ SwiftfulFirebase/SwiftfulFirebaseApp.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftfulFirebaseApp.swift
+//  SwiftfulFirebase
+//
+//  Created by Ahmed Ali on 23/03/2023.
+//
+
+import Firebase
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseApp: App {
+    init() {
+        FirebaseApp.configure()
+        print("Firebase configured")
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
